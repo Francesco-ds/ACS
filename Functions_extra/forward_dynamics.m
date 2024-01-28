29b11616011bb8053f0f89d4f69ab5c6d5f98320
Francesco-ds <58468024+Francesco-ds@users.noreply.github.com>
2024-01-28 15:35:03 +0100

    Add files via upload

--- Functions_extra/forward_dynamics.m
+++ Functions_extra/forward_dynamics.m
@@ -43,8 +43,8 @@ end
 function [sys,x0,str,ts]=mdlInitializeSizes(n, qi, dqi)
 
 sizes = simsizes;
-sizes.NumContStates  = 2*n;%q,qdot for each joint
-sizes.NumDiscStates  = 0;
+sizes.NumContStates  = 0;%q,qdot for each joint %swapped
+sizes.NumDiscStates  = 6;
 sizes.NumOutputs     = 2*n; 
 sizes.NumInputs      = n;%n dof
 sizes.DirFeedthrough = 0;%no direct link to input and output 
@@ -52,7 +52,6 @@ sizes.NumSampleTimes = 1; %should be 1 to avoid errors
 
 sys = simsizes(sizes);
 x0  = [qi; dqi];  %starting positions and velocities
-%x0  = [qi + [0.5,0.1,-0.2]'; dqi];
 str = [];
 ts  = [0 0];
 
@@ -67,6 +66,7 @@ function sys=mdlDerivatives(t,x,u,robot)
     N = 3;
     q = x(1:N);
     dq = x(N+1:2*N);
+    assert(all(imag(u)==0), 'u is imaginary or nan');
     ddq = forw_dyn_val(q, dq, u); %extra function to get the acceleration to give in output the acc, inp = pos vel tau
     sys = [dq; ddq]; %sys vars
     
